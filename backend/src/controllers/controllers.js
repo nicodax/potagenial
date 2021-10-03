1a87f053edcd0e2834d1f7fca47b30a182b970fe
daxxramass <he201753@students.ephec.be>
2021-10-03 13:48:03 +0200

    [api] error handling

--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -8,11 +8,16 @@ const getUser = (req, res) => {
     } else {
         const sqlQuery = `SELECT * FROM users WHERE user_username = '${req.params.username}'`;
 
-        database.query(sqlQuery, (err, result) => {
-            if (err) throw err;
-
-            res.json(result);
-        });
+        try {
+            database.query(sqlQuery, (err, result) => {
+                if (err) res.status(520);
+                
+                res.json(result);
+            });
+        }
+        catch(err) {
+            res.status(520);
+        }
     }
 };
 
@@ -24,8 +29,8 @@ const logUserIn = (req, res) => {
         const sqlQuery = `SELECT * FROM users WHERE user_username = '${req.body.username}' AND user_password = '${req.body.password}'`;
 
         database.query(sqlQuery, (err, result) => {
-            if (err) throw err;
-
+            if (err) res.status(520);
+            
             res.json(result);
         });
     }
@@ -39,7 +44,7 @@ const signUserIn = (req, res) => {
         const sqlQuery = `INSERT INTO users (user_username, user_password, user_firstname, user_lastname, user_email, user_birthdate, user_sexe, user_country, user_city, user_address, user_house_number, user_zipcode) VALUES ('${req.body.username}', '${req.body.password}', '${req.body.firstname}', '${req.body.lastname}', '${req.body.email}', '${req.body.birthdate}', '${req.body.sexe}', '${req.body.country}', '${req.body.city}', '${req.body.address}', ${req.body.house_number}, ${req.body.zipcode});`;
 
         database.query(sqlQuery, (err, result) => {
-            if (err) throw err;
+            if (err) res.status(400);
             
             res.json(result);
         });
@@ -54,7 +59,7 @@ const amendPwd = (req, res) => {
         const sqlQuery = `UPDATE users SET user_password = '${req.body.password}' where user_username = '${req.body.username}'`;
 
         database.query(sqlQuery, (err, result) => {
-            if (err) throw err;
+            if (err) res.status(400);
             
             res.json(result);
         });
@@ -69,7 +74,7 @@ const amendName = (req, res) => {
         const sqlQuery = `UPDATE users SET user_firstname = '${req.body.firstname}', user_lastname = '${req.body.lastname}' where user_username = '${req.body.username}'`;
 
         database.query(sqlQuery, (err, result) => {
-            if (err) throw err;
+            if (err) res.status(400);
             
             res.json(result);
         });
@@ -84,7 +89,7 @@ const amendEmail = (req, res) => {
         const sqlQuery = `UPDATE users SET user_email = '${req.body.email}' where user_username = '${req.body.username}'`;
 
         database.query(sqlQuery, (err, result) => {
-            if (err) throw err;
+            if (err) res.status(400);
             
             res.json(result);
         });
@@ -99,7 +104,7 @@ const amendAddress = (req, res) => {
         const sqlQuery = `UPDATE users SET user_country = '${req.body.country}', user_city = '${req.body.city}', user_address = '${req.body.address}', user_house_number = ${req.body.house_number}, user_zipcode = ${req.body.zipcode} where user_username = '${req.body.username}'`;
 
         database.query(sqlQuery, (err, result) => {
-            if (err) throw err;
+            if (err) res.status(400);
             
             res.json(result);
         });
