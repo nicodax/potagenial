6c34948adce3dbbbcf9ff68a74e2cb49dc5e1eef
noellekh <63249943+noellekh@users.noreply.github.com>
2021-10-26 14:20:28 +0200

    onglet live camera

--- frontend/app/src/main/java/ovh/daxhelet/potagenial/CameraActivity.java
+++ frontend/app/src/main/java/ovh/daxhelet/potagenial/CameraActivity.java
@@ -1,14 +1,66 @@
 package ovh.daxhelet.potagenial;
 
+import android.net.Uri;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class CameraActivity extends AppCompatActivity {
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.util.VLCVideoLayout;
+
+
+public class CameraActivity extends AppCompatActivity
+{
+    private static final String rtspUrl = "rtsp://root:ipcam@192.168.1.22:554/live.sdp";
+
+    private LibVLC libVlc;
+    private MediaPlayer mediaPlayer;
+    private VLCVideoLayout videoLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        setContentView(R.layout.activity_main);
+
+        libVlc = new LibVLC(this);
+        mediaPlayer = new MediaPlayer(libVlc);
+        videoLayout = findViewById(R.id.videoLayout);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        mediaPlayer.attachViews(videoLayout, null, false, false);
+
+        Media media = new Media(libVlc, Uri.parse(rtspUrl));
+        media.setHWDecoderEnabled(true, false);
+        media.addOption(":network-caching=600");
+
+        mediaPlayer.setMedia(media);
+        media.release();
+        mediaPlayer.play();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+
+        mediaPlayer.stop();
+        mediaPlayer.detachViews();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        mediaPlayer.release();
+        libVlc.release();
     }
 }
