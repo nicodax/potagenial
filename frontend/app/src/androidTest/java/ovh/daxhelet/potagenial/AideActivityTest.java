0d9e2b7b5ee02c9a7502fc17c4e2bc93addddb95
noellekh <63249943+noellekh@users.noreply.github.com>
2021-12-08 22:40:16 +0100

    Unit test SignUp form

--- frontend/app/src/androidTest/java/ovh/daxhelet/potagenial/AideActivityTest.java
+++ frontend/app/src/androidTest/java/ovh/daxhelet/potagenial/AideActivityTest.java
@@ -1,4 +1,41 @@
+package ovh.daxhelet.potagenial;
+
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.junit.Assert.*;
+
+import android.app.Instrumentation;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.espresso.matcher.ViewMatchers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
 public class AideActivityTest {
-  
+
+    @Rule
+    public IntentsTestRule<AideActivity> aideActivityIntentsTestRule = new IntentsTestRule<>(AideActivity.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+
+    public void sendEmailTest(){
+        Espresso.onView(ViewMatchers.withId(R.id.bouton)).perform(ViewActions.click());
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
--- frontend/app/src/androidTest/java/ovh/daxhelet/potagenial/SignupActivityTest.java
+++ frontend/app/src/androidTest/java/ovh/daxhelet/potagenial/SignupActivityTest.java
@@ -1,4 +1,52 @@
+package ovh.daxhelet.potagenial;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.w3c.dom.Text;
+
 public class SignupActivityTest {
-  
+    @Rule
+    public ActivityTestRule<SignupActivity>signupActivityActivityTestRule=new ActivityTestRule<SignupActivity>(SignupActivity.class);
+
+    private String eUsername = "jac00k";
+    private String eEmail = "jack0000@hotmail.com";
+    private String ePassword = "coucou";
+    private String eConfirmPassword = "coucou";
+    private String eFirstname = "jack";
+    private String eLastname = "HJ";
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public  void userInput() {
+        Espresso.onView(withId(R.id.etSignupUsername)).perform(typeText(eUsername));
+        Espresso.onView(withId(R.id.etSignupEmail)).perform(typeText(eEmail));
+        Espresso.onView(withId(R.id.etSignupPassword)).perform(typeText(ePassword));
+        Espresso.onView(withId(R.id.etSignupConfirmPassword)).perform(typeText(eConfirmPassword));
+        Espresso.onView(withId(R.id.etLastname)).perform(typeText(eLastname));
+        Espresso.onView(withId(R.id.etFirstname)).perform(typeText(eFirstname));
+        Espresso.onView(withId(R.id.btnSignup)).perform(click());
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
