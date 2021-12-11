b7b9e4e600f29c96375f12f51f23125280912afb
nicodax <he201753@students.ephec.be>
2021-12-12 00:00:52 +0100

    cleanup code index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const http = require('http');
 const https = require('https');
-const path = require('path');
 const fs = require('fs');
 
 const app = express();
@@ -19,15 +17,10 @@ const privateKey = fs.readFileSync('/etc/letsencrypt/live/daxhelet.ovh/privkey.p
 const certificate = fs.readFileSync('/etc/letsencrypt/live/daxhelet.ovh/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/daxhelet.ovh/chain.pem', 'utf8');
 
-const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
     key: privateKey,
 	cert: certificate,
 	ca: ca
 }, app)
-
-httpServer.listen(80, () => {
-	console.log('HTTP Server running on port 80');
-});
 
 httpsServer.listen(port, () => console.log(`Server running on port ${port}`));
