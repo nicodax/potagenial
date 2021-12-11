dfc095f1b627f79e6d6d38ebceaba7497a7a85c4
nicodax <he201753@students.ephec.be>
2021-12-11 21:48:44 +0100

    [TT.7] self-signed SSL certificate pour HTTPS

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
 
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3443;
 
 const routes = require('./src/routes/routes');
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
+
+sslServer.listen(port, () => console.log(`Server running on port ${port}`));
