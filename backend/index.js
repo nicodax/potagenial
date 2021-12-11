9aa437568806e60f434e1bdcf99d736790eb6ef7
nicodax <he201753@students.ephec.be>
2021-12-11 23:59:17 +0100

    final test ?

--- backend/index.js
+++ backend/index.js
@@ -12,33 +12,22 @@ const port = process.env.PORT || 3443;
 
 const routes = require('./src/routes/routes');
 
-// app.use(express.json());
-// app.use(routes);
+app.use(express.json());
+app.use(routes);
 
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/daxhelet.ovh/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/daxhelet.ovh/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/daxhelet.ovh/chain.pem', 'utf8');
 
-app.listen(80, () => {
-  console.log('HTTP server running on port 80');
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+    key: privateKey,
+	cert: certificate,
+	ca: ca
+}, app)
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
 });
 
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
-
-// const credentials = {
-// 	key: privateKey,
-// 	cert: certificate,
-// 	ca: ca
-// };
-
-// const httpServer = http.createServer(app);
-// const httpsServer = https.createServer({
-//     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-//     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-// }, app)
-
-// httpServer.listen(80, () => {
-// 	console.log('HTTP Server running on port 80');
-// });
-
-// httpsServer.listen(port, () => console.log(`Server running on port ${port}`));
+httpsServer.listen(port, () => console.log(`Server running on port ${port}`));
